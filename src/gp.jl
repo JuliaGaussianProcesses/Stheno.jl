a772a79a645daa8cfdf93d7c637a99dd225b1199
[A[A[A[B[B[Bwilltebbutt <wt0881@my.bristol.ac.uk>
2017-09-12 00:03:11 +0100

    More unit testing added. Some interface modifications and renaming of some types. Stops short of actually being able to sample from a GP because I haven't yet implemented the ability to construct block covariance matrices and block mean vectors, which are essential to be able to work with multi-output GPs. To make this work, we may need to extend BlockArrays.jl, or we may need to create our own thing if our requirements aren't met by BlockArrays.jl

--- src/gp.jl
+++ src/gp.jl
@@ -1,59 +1,113 @@
-import Base: +, length
+import Base: +, length, getindex, push!, mean, size
+export mean, kernel, GP
+
+"""
+    KernelCollection
+
+An upper triangular matrix represented as a vector of vectors, the pth of whom's length is
+p, which will contain kernels. This is an internal data structure, that the user of the
+package should not need to interact with directly.
+"""
+struct KernelCollection
+    k::Vector{<:Vector{<:Any}}
+    KernelCollection(k::Vector{<:Vector{<:Any}}) = new(k)
+    KernelCollection() = new(Vector{Vector{Any}}())
+end
+getindex(t::KernelCollection, p::Int, q::Int) = p > q ? t.k[p][q] : t.k[q][p]
+push!(t::KernelCollection, x::Vector{<:Any}) = (push!(t.k, x); t)
+size(t::KernelCollection) = (length(t.k), length(t.k))
+size(t::KernelCollection, d::Int) =
+    d < 1 ? throw(error("arraysize: dim out of bounds.")) : (d < 3 ? length(t.k) : 1)
+
+"""
+    GPCollection
+
+A a representation of collection of Gaussian Processes. This is an internal data structure
+that the user of the package should not need to interact with directly.
+"""
+struct GPCollection
+    μ::Vector{<:Any}
+    k::KernelCollection
+    GPCollection() = new(Vector{Any}(), KernelCollection())
+    function GPCollection(μ::Vector{<:Any}, k::KernelCollection)
+        length(μ) != size(k, 1) && throw(ArgumentError("μ and k have different sizes."))
+        return new(μ, k)
+    end
+end
+
+"""
+    length(gpc::GPCollection)
+
+The number of Gaussian Processes represented by `gpc`.
+"""
+length(gpc::GPCollection) = length(gpc.μ)
+
+"""
+    mean(gpc::GPCollection, p::Int)
+
+Mean function of the `p`th GP in `gpc`.
+"""
+mean(gpc::GPCollection, p::Int) = gpc.μ[p]
+
+"""
+    kernel(gpc::GPCollection, p::Int, q::Int)
+    kernel(gpc::GPCollection, p::Int)
+
+Get the cross-covariance between the `p`th and `q`th processes in `gpc`. If only a single
+index `p` is provided, or `p == q`, then the covariance function of the `p`th process in
+`gpc` is returned.
+"""
+kernel(gpc::GPCollection, p::Int, q::Int) = gpc.k[p, q]
+kernel(gpc::GPCollection, p::Int) = kernel(gpc, p, p)
+
+"""
+    append_indep!(gp::GPCollection, μ, k::Kernel)
+
+Append an independent GP onto `gp`, with mean function `μ` and kernel `k`.
+"""
+function append_indep!(gpc::GPCollection, μ, k::Kernel)
+    len = length(gpc)
+    push!(gpc.μ, μ)
+    push!(gpc.k, vcat(fill(Constant(0.0), len), k))
+    return gpc
+end
 
 """
     GP
 
-A marginal Gaussian Process object. Points to a JointGP object which contains mean and
+A marginal Gaussian Process object. Points to a GPCollection object which contains mean and
 covariance function information.
 """
 struct GP
     idx::Int
-    joint::JointGP
+    joint::GPCollection
+    GP(idx::Int, jgp::GPCollection) = new(idx, jgp)
+    function GP(jgp::GPCollection, μ, k::Kernel)
+        jgp = append_indep!(jgp, μ, k)
+        return new(length(jgp), jgp)
+    end
 end
-
-"""
-    JointKernel
-"""
-struct JointKernel
-    k::Vector{Vector{Any}}
-end
-getindex(t::JointKernel, p::Int, q::Int) = p > q ? t.xcov[q][p] : t.xcov[p][q]
-
-"""
-    JointGP
-
-A tape-like object which keeps track of the joint distribution over the GPs.
-"""
-struct JointGP
-    gps::Vector{GP}
-    μ::Vector{Any}
-    k::JointKernel
-end
-length(t::JointGP) = length(t.gps)
 
 """
     mean(gp::GP)
-    mean(gp::JointGP, n::Int)
 
-Return the mean function of the `n^{th}` `GP` in a `JointGP`, or the mean function of a
-marginal `GP`.
+Get the mean function of the marginal `GP` `gp`.
 """
-mean(gp::JointGP, n::Int) = gp.μ[n]
-mean(gp::JointGP) = gp.μ
 mean(gp::GP) = mean(gp.joint, gp.idx)
 
 """
+    kernel(gp1::GP, gp2::GP)
     kernel(gp::GP)
-    kernel(gp::JointGP)
-    kernel(gp::JointGP, p::Int, q::Int)
 
-Return the kernel of the `(p,q)^{th}` `GP` in a `JointGP`, or the kernel of a marginal `GP`.
-If no indices are provided and a `JointGP` is provided, then the `JointKernel` object is
-returned.
+Get the cross-covariance function between `GP`s `gp1` and `gp2`. If only a single `GP` is
+provided, the "marginal" covariance function associated with this `GP` is returned. If
+`gp1` and `gp2` point towards different `GPCollection`s, an assertion will fail.
 """
-kernel(gp::GP) = gp.joint[gp.idx, gp.idx]
-kernel(gp::JointGP) = gp.k
-kernel(gp::JointGP, p::Int, q::Int) = kernel(gp)[p, q]
+@inline function kernel(gp1::GP, gp2::GP)
+    @assert gp1.joint === gp2.joint
+    return kernel(gp1.joint, gp1.idx, gp2.idx)
+end
+kernel(gp::GP) = kernel(gp.joint, gp.idx)
 
 """
     lml(gp::GP, x::AbstractVector, y::AbstractVector)
@@ -77,35 +131,28 @@ sample(rng::AbstractRNG, gp::GP, x::AbstractVector, N::Int=1) =
 
 Return the process that one obtains when two GPs are added together, and compute all of the
 information necessary to perform inference in the joint distribution over the GPs.
+If `p1` and `p2` point towards different `GPCollection`s, an assertion will fail.
 """
 function +(p1::GP, p2::GP)
 
-    # Make sure the JointKernel is common to both processes and extract it.
-    p1.k != p2.k && throw(error("Tapes don't match, can't add GPs."))
-    joint_kernel = p1.k
+    # Make sure the KernelCollection is common to both processes and extract it.
+    @assert p1.joint === p2.joint
+    joint, p, q = p1.joint, p1.idx, p2.idx
 
-    # Compute the cross-kernels between this process and each other process + itself.
-    p, q, N = p1.p, p2.p, length(joint_kernel.gps)
+    # Compute and push the mean function of the new GP.
+    μ = mean(joint)
+    μp, μq = μ[p], μ[q]
+    push!(μ, x->μp(x) + μq(x))
+
+    # Compute + push the cross-kernels between this process and each other process + itself.
+    k, p, q, N = kernel(joint), p1.idx, p2.idx, length(joint)
     ks = Vector{Kernel}(N + 1)
     for t in 1:N
-        ks[p] = joint_kernel[t, p] + joint_kernel[t, q]
+        ks[p] = k[t, p] + k[t, q]
     end
-    ks[end] = joint_kernel[p, p] + joint_kernel[q, q] + 2 * joint_kernel[p, q]
-    push!(joint_kernel.xcov, ks)
+    ks[end] = k[p, p] + k[q, q] + 2 * k[p, q]
+    push!(k, ks)
 
-    # Create new GP, append the JointKernel, and return the new GP.
-    p_new = GP(x->p1.μ(x) + p2.μ(x), N + 1, joint_kernel)
-    push!(joint_kernel.gps, p_new)
-    return p_new
+    # Create and return the new GP.
+    return GP(joint, N + 1)
 end
-
-# NEED TO DEFINE THE ZERO KERNEL! AND THE CLOSURE NEEDS TO BE WORKED OUT, BECAUSE THE NEW
-# PROCESS WILL INHERIT COVARIANCE PROPERTIES FROM PREVIOUS PROCESSES!
-
-# Also figure out how the bloody hell to get Nabla to work with this properly. Essentially
-# we need sensible functionality for quite a number of types (Symettric, StidedPDMat in
-# particular, but other things will be necessary. Now that we've dropped the requirement that
-# typeof(X̄) == typeof(X), we can do much more sophisticated things, and implement custom
-# behaviour where necessary. We will require more sophisticated implementations for kernel
-# sensitivities once we move to the multi-dimensional setting and broadcasts no longer work
-# as intended).
--- test/gp.jl
+++ test/gp.jl
@@ -1,3 +1,105 @@
 @testset "gp" begin
-    
+
+    # Test basics for `KernelCollection` container.
+    import Stheno.KernelCollection
+    let rng = MersenneTwister(123456)
+
+        r1, r2, r3 = randn(rng, 5), randn(rng, 6), randn(rng, 7)
+        jk = KernelCollection([r1, r2, r3])
+
+        @test jk[1, 1] === r1[1]
+        @test jk[1, 2] === r2[1]
+        @test jk[2, 1] === jk[1, 2]
+        @test jk[3, 3] === r3[3]
+        @test_throws BoundsError jk[1, 4]
+        @test_throws BoundsError jk[4, 1]
+        @test size(jk) == (3, 3)
+        @test size(jk, 1) == 3
+
+        jk_ext = push!(jk, r1)
+        @test jk_ext[1, 4] === r1[1]
+        @test jk_ext[4, 1] === jk_ext[1, 4]
+        @test size(jk_ext) == (4, 4)
+        @test size(jk_ext, 2) == 4
+    end
+
+    # Test `length`, `mean`, `kernel` for `GPCollection`.
+    import Stheno.GPCollection
+    let rng = MersenneTwister(123456)
+
+        r1, r2, r3 = randn(rng, 5), randn(rng, 6), randn(rng, 7)
+        μ, jk = randn(rng, 3), KernelCollection([r1, r2, r3])
+        @test_throws ArgumentError GPCollection(randn(rng, 4), jk)
+        gp = GPCollection(μ, jk)
+
+        @test length(gp) == 3
+        @test mean(gp, 1) === μ[1]
+        @test kernel(gp, 1) === jk[1, 1]
+        @test kernel(gp, 1, 1) == jk[1, 1]
+        @test kernel(gp, 1, 2) == jk[1, 2]
+    end
+
+    # Test `append_indep!`.
+    import Stheno.append_indep!
+    let
+        # Specification for three independent GPs.
+        μ1, μ2, μ3 = sin, cos, tan
+        k1, k2, k3 = EQ(), RQ(10.0), RQ(1.0)
+
+        # Make a GPCollection and append stuff to it.
+        gpc = GPCollection()
+        append_indep!(gpc, μ1, k1)
+        append_indep!(gpc, μ2, k2)
+        append_indep!(gpc, μ3, k3)
+
+        # Check mean function vector.
+        @test mean(gpc, 1) == sin
+        @test mean(gpc, 2) == cos
+        @test mean(gpc, 3) == tan
+
+        # Check on-diagonal covariance functions.
+        @test kernel(gpc, 1) == EQ()
+        @test kernel(gpc, 2) == RQ(10.0)
+        @test kernel(gpc, 3) == RQ(1.0)
+
+        # Check off-diagonal covariance functions.
+        @test kernel(gpc, 1, 2) == Constant(0.0)
+        @test kernel(gpc, 1, 3) == Constant(0.0)
+        @test kernel(gpc, 2, 3) == Constant(0.0)
+    end
+
+    # Check invariances for construction of a set of independent GPs.
+    let
+        # Specification for three independent GPs.
+        μ1, μ2, μ3 = sin, cos, tan
+        k1, k2, k3 = EQ(), RQ(10.0), RQ(1.0)
+
+        # Build a GPCollection.
+        gpc = GPCollection()
+        gp1 = GP(gpc, μ1, k1)
+        gp2 = GP(gpc, μ2, k2)
+        gp3 = GP(gpc, μ3, k3)
+
+        # Check mean functions.
+        @test mean(gp1) == sin
+        @test mean(gp2) == cos
+        @test mean(gp3) == tan
+
+        # Check covariance functions.
+        @test kernel(gp1) == EQ()
+        @test kernel(gp2) == RQ(10.0)
+        @test kernel(gp3) == RQ(1.0)
+
+        # Check cross-covariance functions.
+        @test kernel(gp1, gp2) == Constant(0.0)
+        @test kernel(gp1, gp3) == Constant(0.0)
+        @test kernel(gp2, gp3) == Constant(0.0)
+
+        # Build a different GPCollection and make sure that an assertion is thrown
+        # when trying to index into different GPCollections.
+        gpc2 = GPCollection()
+        gp_different = GP(gpc2, μ1, k1)
+        @test_throws AssertionError kernel(gp1, gp_different)
+    end
+
 end
