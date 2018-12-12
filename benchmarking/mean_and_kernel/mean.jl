8282369528338f82b927b7d1e92136136d4d3fd0
Will Tebbutt <wt0881@my.bristol.ac.uk>
2018-12-12 17:47:39 +0000

    Some work

--- benchmarking/mean_and_kernel/mean.jl
+++ benchmarking/mean_and_kernel/mean.jl
@@ -1,13 +1,11 @@
 function mean_benchmarks()
     suite = BenchmarkGroup()
-    # suite["ZeroMean{Float64}()"] = create_benchmarks(ZeroMean{Float64}())
+    suite["ZeroMean{Float64}()"] = create_benchmarks(ZeroMean{Float64}())
     # suite["ConstantMean(5.0)"] = create_benchmarks(ConstantMean(5.0))
-    suite["CustomMean(sin)"] = create_benchmarks(CustomMean(sin))
-    suite["CustomMean(cos)"] = create_benchmarks(CustomMean(cos))
+    # suite["CustomMean(sin)"] = create_benchmarks(CustomMean(sin))
+    # suite["CustomMean(cos)"] = create_benchmarks(CustomMean(cos))
     return suite
 end
-
-const mean_Ns = [10, 100, 100, 10_000]
 
 function create_benchmarks(μ::MeanFunction)
     suite = BenchmarkGroup()
@@ -15,7 +13,7 @@ function create_benchmarks(μ::MeanFunction)
     suite["μ(x)"] = @benchmarkable $μ(5.0)
     suite["gradient(μ, x)"] = @benchmarkable Zygote.gradient($μ, 5.0)
 
-    for N in mean_Ns
+    for N in [10, 100, 100, 10_000]
         x = randn(N)
         suite["map ($N)"] = @benchmarkable map($μ, $x)
         suite["gradient map ($N)"] = @benchmarkable Zygote.gradient(x->sum(map($μ, x)), $x)
