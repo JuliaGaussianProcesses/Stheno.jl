ead8cd4056ad12ff8cf49bfc9691998fe9231dc2
Will Tebbutt <wt0881@my.bristol.ac.uk>
2019-04-08 22:55:36 +0100

    Some work

--- test/linops/indexing.jl
+++ test/linops/indexing.jl
@@ -2,8 +2,18 @@ using Random, LinearAlgebra
 using Stheno: GPC
 
 @testset "indexing" begin
-    f, x = GP(eq(), GPC()), randn(10)
-    fx = f(x)
-    @test mean(fx) == map(mean(f), x)
-    @test cov(fx) == pairwise(kernel(f), x)
+    @testset "no noise" begin
+        f, x = GP(eq(), GPC()), randn(10)
+        fx = f(x)
+        @test mean(fx) == map(mean(f), x)
+        @test cov(fx) == pairwise(kernel(f), x)
+    end
+    @testset "matrix-valued noise" begin
+        rng, N = MersenneTwister(123456), 10
+        f, x, A = GP(eq(), GPC()), randn(rng, N), randn(rng, N, N)
+        C = Symmetric(A * A' + I)
+        fx = f(x, C)
+        @test mean(fx) == map(mean(f), x)
+        @test cov(fx) == pairwise(kernel(f), x) + C
+    end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,34 +4,34 @@ include("test_util.jl")
 
 @testset "Stheno" begin
 
-    @testset "util" begin
-        include("util/zygote_rules.jl")
-        include("util/covariance_matrices.jl")
-        include("util/block_arrays.jl")
-        include("util/abstract_data_set.jl")
-        include("util/fillarrays.jl")
-    end
+    # @testset "util" begin
+    #     include("util/zygote_rules.jl")
+    #     include("util/covariance_matrices.jl")
+    #     include("util/block_arrays.jl")
+    #     include("util/abstract_data_set.jl")
+    #     include("util/fillarrays.jl")
+    # end
 
-    @testset "mean_and_kernel" begin
-        include("mean_and_kernel/mean.jl")
-        include("mean_and_kernel/kernel.jl")
-        include("mean_and_kernel/compose.jl")
-        include("mean_and_kernel/block.jl")
-        include("mean_and_kernel/input_transform.jl")
-        # include("mean_and_kernel/derivative.jl") # These tests currenly fail because Zygote.
-        @testset "conditioning" begin
-            include("mean_and_kernel/conditioning/exact.jl")
-            include("mean_and_kernel/conditioning/titsias.jl")
-        end
-        include("mean_and_kernel/algebra.jl")
-        include("mean_and_kernel/util.jl")
-    end
+    # @testset "mean_and_kernel" begin
+    #     include("mean_and_kernel/mean.jl")
+    #     include("mean_and_kernel/kernel.jl")
+    #     include("mean_and_kernel/compose.jl")
+    #     include("mean_and_kernel/block.jl")
+    #     include("mean_and_kernel/input_transform.jl")
+    #     # include("mean_and_kernel/derivative.jl") # These tests currenly fail because Zygote.
+    #     @testset "conditioning" begin
+    #         include("mean_and_kernel/conditioning/exact.jl")
+    #         include("mean_and_kernel/conditioning/titsias.jl")
+    #     end
+    #     include("mean_and_kernel/algebra.jl")
+    #     include("mean_and_kernel/util.jl")
+    # end
 
-    @testset "gp" begin
-        include("gp/gp.jl")
-        include("gp/block_gp.jl")
-        include("gp/finite_gp.jl")
-    end
+    # @testset "gp" begin
+    #     include("gp/gp.jl")
+    #     include("gp/block_gp.jl")
+    #     include("gp/finite_gp.jl")
+    # end
 
     @testset "linops" begin
         include("linops/indexing.jl")
