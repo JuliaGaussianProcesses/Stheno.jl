d73fd001032b4d01709bcb24fe9a8d51b1254ecf
willtebbutt <wt0881@my.bristol.ac.uk>
2019-08-20 08:06:03 +0100

    Also require that CI passes for Julia 1.2

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,36 +8,36 @@ include("test_util.jl")
 
 @testset "Stheno" begin
 
-    # println("testing util")
-    # a = time()
-    # @testset "util" begin
-    #     include(joinpath("util", "zygote_rules.jl"))
-    #     include(joinpath("util", "covariance_matrices.jl"))
-    #     @testset "block_arrays" begin
-    #         include(joinpath("util", "block_arrays", "test_util.jl"))
-    #         include(joinpath("util", "block_arrays", "dense.jl"))
-    #         include(joinpath("util", "block_arrays", "diagonal.jl"))
-    #     end
-    #     include(joinpath("util", "abstract_data_set.jl"))
-    # end
-    # println("testing util took $(time() - a)s")
+    println("testing util")
+    a = time()
+    @testset "util" begin
+        include(joinpath("util", "zygote_rules.jl"))
+        include(joinpath("util", "covariance_matrices.jl"))
+        @testset "block_arrays" begin
+            include(joinpath("util", "block_arrays", "test_util.jl"))
+            include(joinpath("util", "block_arrays", "dense.jl"))
+            include(joinpath("util", "block_arrays", "diagonal.jl"))
+        end
+        include(joinpath("util", "abstract_data_set.jl"))
+    end
+    println("testing util took $(time() - a)s")
 
-    # println("testing mean_and_kernel")
-    # a = time()
-    # @testset "mean_and_kernel" begin
-    #     include(joinpath("mean_and_kernel", "mean.jl"))
-    #     include(joinpath("mean_and_kernel", "kernel.jl"))
-    # end
-    # println("mean_and_kernel took $(time() - a)s")
+    println("testing mean_and_kernel")
+    a = time()
+    @testset "mean_and_kernel" begin
+        include(joinpath("mean_and_kernel", "mean.jl"))
+        include(joinpath("mean_and_kernel", "kernel.jl"))
+    end
+    println("mean_and_kernel took $(time() - a)s")
 
 
-    # println("testing gp")
-    # a = time()
-    # @testset "gp" begin
-    #     include(joinpath("gp", "gp.jl"))
-    #     include(joinpath("gp", "finite_gp.jl"))
-    # end
-    # println("testing gp took $(time() - a)s")
+    println("testing gp")
+    a = time()
+    @testset "gp" begin
+        include(joinpath("gp", "gp.jl"))
+        include(joinpath("gp", "finite_gp.jl"))
+    end
+    println("testing gp took $(time() - a)s")
 
     println("linops:")
     @time @testset "linops" begin
