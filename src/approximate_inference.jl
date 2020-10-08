44e25bc2c5f15470350a0fb7d92a9330e1ec6f87
Sam Urmy <sam.urmy@noaa.gov>
2020-10-08 10:05:04 -0700

    Delete comment

--- src/approximate_inference.jl
+++ src/approximate_inference.jl
@@ -11,9 +11,6 @@ end
 
 length(f::SparseFiniteGP) = length(f.fobs)
 mean(f::SparseFiniteGP) = mean(f.fobs)
-# in Base, trying to instantiate a large dense matrix through e.g.
-# inv(A::SparseArrays.AbstractSparseMatrixCSC) raises an error, since the resulting dense
-# matrix will often be enormous and use up all available memory. Doing something similar here.
 covariance_error = "The covariance matrix of a sparse GP can often be dense and " *
     "can cause the computer to run out of memory. If you are sure you have enough " *
     "memory, you can use `cov(f.fobs)`."
