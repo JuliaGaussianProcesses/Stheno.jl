02573508f12af163cb61c7b182d7f53732f5565d
wt <wt0881@my.bristol.ac.uk>
2020-10-16 10:52:48 +0100

    Uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,59 +25,59 @@ include("test_util.jl")
 
 @testset "Stheno" begin
 
-    # println("util:")
-    # @timedtestset "util" begin
-    #     include(joinpath("util", "zygote_rules.jl"))
-    #     include(joinpath("util", "covariance_matrices.jl"))
-    #     @testset "block_arrays" begin
-    #         include(joinpath("util", "block_arrays", "test_util.jl"))
-    #         include(joinpath("util", "block_arrays", "dense.jl"))
-    #         include(joinpath("util", "block_arrays", "diagonal.jl"))
-    #     end
-    #     include(joinpath("util", "abstract_data_set.jl"))
-    #     include(joinpath("util", "distances.jl"))
-    # end
+    println("util:")
+    @timedtestset "util" begin
+        include(joinpath("util", "zygote_rules.jl"))
+        include(joinpath("util", "covariance_matrices.jl"))
+        @testset "block_arrays" begin
+            include(joinpath("util", "block_arrays", "test_util.jl"))
+            include(joinpath("util", "block_arrays", "dense.jl"))
+            include(joinpath("util", "block_arrays", "diagonal.jl"))
+        end
+        include(joinpath("util", "abstract_data_set.jl"))
+        include(joinpath("util", "distances.jl"))
+    end
 
-    # println("gp:")
-    # @timedtestset "gp" begin
-    #     include(joinpath("gp", "mean.jl"))
-    #     include(joinpath("gp", "kernel.jl"))
-    #     include(joinpath("gp", "gp.jl"))
-    # end
+    println("gp:")
+    @timedtestset "gp" begin
+        include(joinpath("gp", "mean.jl"))
+        include(joinpath("gp", "kernel.jl"))
+        include(joinpath("gp", "gp.jl"))
+    end
 
-    # println("composite:")
-    # @timedtestset "composite" begin
-    #     include(joinpath("composite", "test_util.jl"))
-    #     include(joinpath("composite", "indexing.jl"))
-    #     include(joinpath("composite", "cross.jl"))
-    #     include(joinpath("composite", "conditioning.jl"))
-    #     include(joinpath("composite", "product.jl"))
-    #     include(joinpath("composite", "addition.jl"))
-    #     include(joinpath("composite", "compose.jl"))
-    #     include(joinpath("composite", "approximate_conditioning.jl"))
-    # end
+    println("composite:")
+    @timedtestset "composite" begin
+        include(joinpath("composite", "test_util.jl"))
+        include(joinpath("composite", "indexing.jl"))
+        include(joinpath("composite", "cross.jl"))
+        include(joinpath("composite", "conditioning.jl"))
+        include(joinpath("composite", "product.jl"))
+        include(joinpath("composite", "addition.jl"))
+        include(joinpath("composite", "compose.jl"))
+        include(joinpath("composite", "approximate_conditioning.jl"))
+    end
 
-    # println("abstract_gp:")
-    # @timedtestset "abstract_gp" begin
-    #     include("abstract_gp.jl")
-    #     include("approximate_inference.jl")
-    # end
+    println("abstract_gp:")
+    @timedtestset "abstract_gp" begin
+        include("abstract_gp.jl")
+        include("approximate_inference.jl")
+    end
 
-    # println("flux:")
-    # @timedtestset "flux" begin
-    #     include(joinpath("flux", "neural_kernel_network.jl"))
-    # end
+    println("flux:")
+    @timedtestset "flux" begin
+        include(joinpath("flux", "neural_kernel_network.jl"))
+    end
 
-    # println("doctests:")
-    # @timedtestset "doctests" begin
-    #     DocMeta.setdocmeta!(
-    #         Stheno,
-    #         :DocTestSetup,
-    #         :(using Stheno, Random, Documenter, LinearAlgebra);
-    #         recursive=true,
-    #     )
-    #     doctest(Stheno)
-    # end
+    println("doctests:")
+    @timedtestset "doctests" begin
+        DocMeta.setdocmeta!(
+            Stheno,
+            :DocTestSetup,
+            :(using Stheno, Random, Documenter, LinearAlgebra);
+            recursive=true,
+        )
+        doctest(Stheno)
+    end
 
     println("turing:")
     @timedtestset "turing" begin
