53763be1ea39a903f790ed3e065e64cb346359d2
WT <wt0881@my.bristol.ac.uk>
2021-03-28 13:45:26 +0100

    Add doctest for GPPPInput

--- src/gaussian_process_probabilistic_programme.jl
+++ src/gaussian_process_probabilistic_programme.jl
@@ -16,6 +16,22 @@ const GPPP = GaussianProcessProbabilisticProgramme
 An collection of inputs for a GPPP.
 `p` indicates which process the vector `x` should be extracted from.
 The required type of `p` is determined by the type of the keys in the `GPPP` indexed.
+
+```jldoctest
+julia> x = [1.0, 1.5, 0.3];
+
+julia> v = GPPPInput(:a, x)
+3-element GPPPInput{Symbol, Float64, Vector{Float64}}:
+ (:a, 1.0)
+ (:a, 1.5)
+ (:a, 0.3)
+
+julia> v isa AbstractVector{Tuple{Symbol, Float64}}
+true
+
+julia> v == map(x_ -> (:a, x_), x)
+true
+```
 """
 struct GPPPInput{Tp, T, Tx<:AbstractVector{T}} <: AbstractVector{Tuple{Tp, T}}
     p::Tp
