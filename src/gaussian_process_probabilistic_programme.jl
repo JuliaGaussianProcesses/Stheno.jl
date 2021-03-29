66e9c062be472d6f88579845db225c2c97ab6afe
WT <wt0881@my.bristol.ac.uk>
2021-03-29 21:08:24 +0100

    Readjust doctests spacing

--- src/gaussian_process_probabilistic_programme.jl
+++ src/gaussian_process_probabilistic_programme.jl
@@ -23,12 +23,12 @@ The required type of `p` is determined by the type of the keys in the `GPPP` ind
 julia> x = [1.0, 1.5, 0.3];
 
 julia> v = GPPPInput(:a, x)
-3-element GPPPInput{Symbol,Float64,Vector{Float64}}:
+3-element GPPPInput{Symbol, Float64, Vector{Float64}}:
  (:a, 1.0)
  (:a, 1.5)
  (:a, 0.3)
 
-julia> v isa AbstractVector{Tuple{Symbol,Float64}}
+julia> v isa AbstractVector{Tuple{Symbol, Float64}}
 true
 
 julia> v == map(x_ -> (:a, x_), x)
