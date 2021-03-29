fdbcbe0697e859c686ba71e32eb5ce847defabf9
WT <wt0881@my.bristol.ac.uk>
2021-03-29 12:23:24 +0100

    Remove composite_gp_api docs

--- docs/make.jl
+++ docs/make.jl
@@ -14,7 +14,6 @@ makedocs(
     pages = [
         "Home" => "index.md",
         "Getting Started" => "getting_started.md",
-        "CompositeGP API" => "composite_gp_api.md",
         "Input Types" => "input_types.md",
         "Kernel Design" => "kernel_design.md",
         "Internals" => "internals.md",
